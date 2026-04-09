341f33f9a3cf267bf315e064eaa1f6045805dba5
nandazulfa16-alt <nandazulfa16@gmail.com>
2026-04-09 19:41:09 +0700

    250441100135_NandaZulfaSeptiana_KakHeema_Modul1

---
+++ modul-1/250441100135_NandaZulfaSeptiana_KakHeema_Modul1/koleksi_langka.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 09, 2026 at 12:36 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `koleksi_langka`
+--
+
+CREATE TABLE `koleksi_langka` (
+  `id_buku` int NOT NULL,
+  `judul` varchar(250) DEFAULT NULL,
+  `tahun_terbit` year DEFAULT NULL,
+  `harga` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `koleksi_langka`
+--
+ALTER TABLE `koleksi_langka`
+  ADD PRIMARY KEY (`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
