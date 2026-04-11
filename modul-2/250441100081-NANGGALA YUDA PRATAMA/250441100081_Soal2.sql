fe770481019f5840f71f81fcc15bd3304491b156
Nanggala Yuda Pratama <nanggalayudhapratama81@gmail.com>
2026-04-12 00:51:34 +0700

    250441100081_NANGGALA YUDA PRATAMA_FIKA NURLAILA_MODUL 2

---
+++ modul-2/250441100081-NANGGALA YUDA PRATAMA/250441100081_Soal2.sql
@@ -0,0 +1,6 @@
+INSERT INTO dokter 
+VALUES (1, 'dr. Andi', 'Umum'), (2, 'dr. Budi', 'Anak');
+INSERT INTO pasien 
+VALUES (101, 'Ani', '1995-03-12', '0812345'), (102, 'Budi', '1990-07-20', '0856789');
+INSERT INTO rekam_medis 
+VALUES (501, 101, 1, '2024-05-01', 'Flu'), (502, 102, 2, '2024-05-02', 'Demam');
